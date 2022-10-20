7635016f933964b0e420e14370b3f6910de4bcce
Jayesh Jadhav <96299946+Jayesh-Jadhav05@users.noreply.github.com>
2022-10-20 12:51:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ ScrollReveal({
  });
 
  ScrollReveal().reveal('.main-point, .common-heading,.skills', { delay: 100, origin: 'left'});
- ScrollReveal().reveal('.section .imgs, .btn,.static-txt', { delay: 100, origin: 'bottom'});
- ScrollReveal().reveal('.second-heading, .social-media-icons,', { delay: 100, origin: 'right'});
- ScrollReveal().reveal('.contact-form, .common-para, .footer-links,', { delay: 100, origin: 'top'});
+ ScrollReveal().reveal('.hero-logo, .btn, .static-txt', { delay: 100, origin: 'bottom'});
+ ScrollReveal().reveal('.second-heading, .social-media-icons', { delay: 100, origin: 'right'});
+ ScrollReveal().reveal('.contact-form, .common-para, .footer-links', { delay: 100, origin: 'top'});
 
 
 //  Responsive navigation bar 
@@ -17,7 +17,12 @@ const mobile_nav = document.querySelector('.mobile-navbar-btn');
 const nav_header = document.querySelector('.head');
 
 
+
+
+
 const toggalbtn = () =>{
     nav_header.classList.toggle('active');
 };
 mobile_nav.addEventListener('click',() => toggalbtn());
+
+
